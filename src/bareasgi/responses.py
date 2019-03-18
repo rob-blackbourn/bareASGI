76ca1ba9313059ea80836b077aee2603b496a28b
Rob Blackbourn <rblackbourn@ugsb-rbla01.bhdgsystematic.com>
2019-03-18 08:42:23 +0000

    Added some documentation comments

--- src/bareasgi/responses.py
+++ src/bareasgi/responses.py
@@ -10,6 +10,15 @@ def bytes_response(
         buf: bytes,
         content_type: bytes
 ) -> HttpResponse:
+    """
+    A helper function to create a bytes response.
+
+    :param status: The HTTP status code.
+    :param headers: The HTTP headers.
+    :param buf: The date to send.
+    :param content_type: The content type of the data.
+    :return: The HTTP response.
+    """
     headers.append((b'content-type', content_type))
     headers.append((b'content-length', str(len(buf)).encode('ascii')))
     return status, headers, bytes_writer(buf)
@@ -22,6 +31,16 @@ def text_response(
         encoding: str = 'utf-8',
         content_type: bytes = b'text/plain'
 ) -> HttpResponse:
+    """
+    A helper function to create a text response.
+
+    :param status: The HTTP status code.
+    :param headers: The HTTP headers.
+    :param text: The text to send.
+    :param encoding: The text encoding (defauults to utf-8).
+    :param content_type: The content type (defaults to text/plain).
+    :return: The HTTP response.
+    """
     return bytes_response(status, headers, text.encode(encoding), content_type)
 
 
@@ -32,4 +51,14 @@ def json_response(
         content_type: bytes = b'application/json',
         dumps=json.dumps
 ) -> HttpResponse:
+    """
+    A helper function to send a json repsonse.
+
+    :param status: The HTTP status code.
+    :param headers: The HTTP headers.
+    :param obj: The object to send as JSON.
+    :param content_type: The content type (defaults to application/json).
+    :param dumps: The function to use to turn the object into JSON (defaults to json.dumps)
+    :return: The HTTP response.
+    """
     return text_response(status, headers, dumps(obj), 'utf-8', content_type)
