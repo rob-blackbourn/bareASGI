3e58d8adf7734ce0449085a13aa6ec403aa6ccc2
Rob Blackbourn <rblackbourn@ugsb-rbla01.bhdgsystematic.com>
2019-05-16 16:10:22 +0100

    Version updated from 1.1.0 to 1.2.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-__version__ = '1.1.0'
+__version__ = '1.2.0'
 
 setup(
     name='bareasgi',
