23395a30a9c8e8227c926e8752ce8f280db0fc16
Rob Blackbourn <rblackbourn@ugsb-rbla01.bhdgsystematic.com>
2019-10-16 11:41:39 +0100

    Added startup and shutdown for shutdown checks

--- examples/server_sent_events_simple.py
+++ examples/server_sent_events_simple.py
@@ -5,20 +5,32 @@ A simple example of server sent events.
 import asyncio
 from datetime import datetime
 import logging
+from typing import Any
+
 from bareasgi import (
     Application,
+    text_writer
+)
+from baretypes import (
     Scope,
     Info,
     RouteMatches,
     Content,
     HttpResponse,
-    text_writer
+    Message
 )
 
 logging.basicConfig(level=logging.DEBUG)
 
-logger = logging.getLogger('server_sent_events')
+logger = logging.getLogger(__name__)
 
+async def on_startup(_scope: Scope, _info: Info, _request: Message) -> None:
+    """Run at startup"""
+    logger.info("Running startup handler")
+
+async def on_shutdown(_scope: Scope, _info: Info, _request: Message) -> None:
+    """Run on shutdown"""
+    logger.info("Running shutdown handler")
 
 # pylint: disable=unused-argument
 async def index(scope: Scope, info: Info, matches: RouteMatches, content: Content) -> HttpResponse:
@@ -77,10 +89,12 @@ async def test_events(
                 yield f'data: {datetime.now()}\n\n\n'.encode('utf-8')
                 # Defeat buffering by giving the server a nudge.
                 yield ':\n\n\n'.encode('utf-8')
-                await asyncio.sleep(1)
+                await asyncio.sleep(60)
             except asyncio.CancelledError:
                 logger.debug('Cancelled')
                 is_cancelled = True
+            except Exception as error:
+                logger.exception('Failed')
 
     headers = [
         (b'cache-control', b'no-cache'),
@@ -92,28 +106,38 @@ async def test_events(
 
 
 if __name__ == "__main__":
-    import uvicorn
-
-    app = Application()
+    app = Application(
+        startup_handlers=[on_startup],
+        shutdown_handlers=[on_shutdown]
+    )
 
     app.http_router.add({'GET'}, '/', index)
     app.http_router.add({'GET'}, '/test', test_page)
     app.http_router.add({'GET'}, '/events', test_events)
 
-    import uvicorn
-    import socket
-    import os.path
-    from hypercorn.asyncio import serve
-    from hypercorn.config import Config
 
-    USE_UVICORN = False
-    hostname = socket.gethostname()  # pylint: disable=invalid-name
+    USE_UVICORN = True
 
     if USE_UVICORN:
+        import uvicorn
         uvicorn.run(app, port=9009)
     else:
+        import signal
+        import uvloop
+        from hypercorn.asyncio import serve
+        from hypercorn.config import Config
+        asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+        shutdown_event = asyncio.Event()
+        def _signal_handler(*_: Any) -> None:
+            shutdown_event.set()
+        loop.add_signal_handler(signal.SIGTERM, _signal_handler)
+        loop.add_signal_handler(signal.SIGINT, _signal_handler)
+
         config = Config()
-        config.bind = [f"{hostname}:9009"]
-        config.certfile = os.path.expanduser(f"~/.keys/{hostname}.crt")
-        config.keyfile = os.path.expanduser(f"~/.keys/{hostname}.key")
-        asyncio.run(serve(app, config))
+        config.bind = ["0.0.0.0:9009"]
+        loop.run_until_complete(
+            serve(app, config, shutdown_trigger=shutdown_event.wait)
+        )
