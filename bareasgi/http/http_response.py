cd4bb0d046e0815360fe4b5094e2d7d492b113e9
Rob Blackbourn <rob.blackbourn@gmail.com>
2022-07-09 11:50:31 +0100

    Added a convenience method

--- bareasgi/http/http_response.py
+++ bareasgi/http/http_response.py
@@ -1,6 +1,11 @@
 """The http response"""
 
-from typing import AsyncIterable, Iterable, List, Optional, Tuple
+from __future__ import annotations
+
+import json
+from typing import Any, AsyncIterable, Callable, Iterable, List, Optional, Tuple
+
+from bareutils import text_writer
 
 PushResponse = Tuple[str, List[Tuple[bytes, bytes]]]
 
@@ -31,3 +36,63 @@ class HttpResponse:
         self.headers = headers
         self.body = body
         self.pushes = pushes
+
+    @classmethod
+    def from_text(
+            cls,
+            text: str,
+            *,
+            status: int = 200,
+            content_type: bytes = b'text/plain',
+            headers: Optional[List[Tuple[bytes, bytes]]] = None
+    ) -> HttpResponse:
+        """Create an HTTP response from a text string.
+
+        Args:
+            text (str): A text string.
+            headers (Optional[List[Tuple[bytes, bytes]]]): Optional headers. Defaults
+                to `None`.
+            status (int, optional): An optional status. Defaults to `200`.
+            content_type (bytes, optional): An optional content type. Defaults
+                to `b'text/plain'`.
+
+        Returns:
+            HttpResponse: The built HTTP response.
+        """
+        return HttpResponse(
+            status,
+            [(b'content-type', content_type)] + (headers or []),
+            text_writer(text)
+        )
+
+    @classmethod
+    def from_json(
+            cls,
+            data: Any,
+            *,
+            status: int = 200,
+            content_type: bytes = b'application/json',
+            headers: Optional[List[Tuple[bytes, bytes]]] = None,
+            dumps: Callable[[Any], str] = json.dumps
+    ) -> HttpResponse:
+        """Create an HTTP response from data converted to JSON.
+
+        Args:
+            data (Any): The data to be converted to JSON.
+            headers (Optional[List[Tuple[bytes, bytes]]]): Optional headers. Defaults
+                to `None`.
+            status (int, optional): An optional status code. Defaults to `200`.
+            content_type (bytes, optional): An optional content type. Defaults
+                to `b'application/json'`.
+            dumps (Callable[[Any], str], optional): An optional function to convert
+                the data to a JSON string. Defaults to `json.dumps`.
+
+        Returns:
+            HttpResponse: _description_
+        """
+        return cls.from_text(
+            dumps(data),
+            status=status,
+            content_type=content_type,
+            headers=headers
+        )
